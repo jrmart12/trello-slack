3c86c8f503b9e33b8d1056a351d40cb02adf2a40
jrmart12 <rodolfojose@live.com>
2021-01-12 16:31:16 -0600

    echo bug

--- main.py
+++ main.py
@@ -5,29 +5,36 @@ import os
 from slackeventsapi import SlackEventAdapter
 
 app = Flask(__name__)
-SLACK_API_KEY = os.environ.get('SLACK_API_KEY')
 slack_web_client = WebClient(token=os.environ['SLACK_API_KEY'])
-class SlackApi:
-    def __init__(self):
-        self.client = WebClient(token=SLACK_API_KEY)
 
-    def message(self, cardName, comment):
-        slack_channel_messages = slack_web_client.conversations_history(channel=cardName)
-        for channel_message in slack_channel_messages['messages']:
-            if(channel_message['text'] != comment):
-                self.client.chat_postMessage(channel="#"+cardName, text=comment)
-                print(
-                    "A message was sent "
-                )                
+def get_channel_name(channel_name):
+        slack_channels = slack_web_client.conversations_list(types="private_channel, public_channel")
+        channel_id = ""
+        for channel in slack_channels['channels']:
+            if(channel['name']==channel_name):
+                channel_id = channel['id']   
+        return channel_id
+
+def message(cardName, comment):
+    slack_channel_messages = slack_web_client.conversations_history(channel=get_channel_name(cardName),limit=1)
+    x = comment.split("(")
+    x_x =  x[0].strip()
+    for channel_message in slack_channel_messages['messages']:
+        if channel_message['text'] == x_x:
+            print("entro")
+            return None
+    slack_web_client.chat_postMessage(channel="#"+cardName, text=comment)
+    print(
+        "A message was sent "
+    )            
 
 @app.route('/webhook', methods=['GET', 'POST']) 
 def webhook():
     data = request.json
     if data["action"]["type"] == "commentCard":
-        slack_api = SlackApi()
         cardName = data["action"]["data"]["card"]["name"]
         comment = data["action"]["data"]["text"]
-        slack_api.message(cardName,comment)
+        message(cardName,comment)
         
     return jsonify(data)
 
