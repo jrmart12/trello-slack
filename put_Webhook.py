3251fd04027d9d97cafefda7ee0df5c4549f5493
jrmart12 <rodolfojose@live.com>
2021-01-12 13:52:37 -0600

    webhook

--- put_Webhook.py
+++ put_Webhook.py
@@ -13,7 +13,7 @@ headers = {
 query = {
    'key': 'ee7b8c0523388294d89c0ba192fa5041',
    'token': '3b6f7d9698ce9552ce5c1ecd9d78617af4388d1581939b967b9a4301adf1d5b5',
-   'callbackURL': 'https://trello-slack-codex.herokuapp.com/webhooks',
+   'callbackURL': 'https://trello-slack-codex.herokuapp.com/webhook',
    'idModel': '5ffcc56fdb5424207a7cf9f9'
 }
 
