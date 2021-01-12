c52ca7b0dba7d59cd2dd991858256bb0850b54c8
jrmart12 <rodolfojose@live.com>
2021-01-12 14:49:53 -0600

    resolve bug

--- main.py
+++ main.py
@@ -2,28 +2,33 @@ from flask import request, redirect, abort, Flask, jsonify
 from slack import WebClient
 import http.client
 import os
+from slackeventsapi import SlackEventAdapter
 
 app = Flask(__name__)
 SLACK_API_KEY = os.environ.get('SLACK_API_KEY')
+slack_web_client = WebClient(token=os.environ['SLACK_API_KEY'])
 class SlackApi:
     def __init__(self):
         self.client = WebClient(token=SLACK_API_KEY)
 
-    def send_message(self, card, slack_message):
-        """Notifies a channel about a new comment via Slack message"""
-        self.client.chat_postMessage(channel="#"+card, text=slack_message)
-        print(
-            "A message was sent "
-        )
+    def message(self, cardName, comment):
+        slack_channel_messages = slack_web_client.conversations_history(channel=cardName)
+        for channel_message in slack_channel_messages['messages']:
+            if(channel_message['text'] != comment):
+                self.client.chat_postMessage(channel="#"+cardName, text=comment)
+                print(
+                    "A message was sent "
+                )                
 
-@app.route('/webhook', methods=['GET', 'POST']) 
+@app.route('/foo', methods=['GET', 'POST']) 
 def webhook():
     data = request.json
     if data["action"]["type"] == "commentCard":
         slack_api = SlackApi()
         cardName = data["action"]["data"]["card"]["name"]
         comment = data["action"]["data"]["text"]
-        slack_api.send_message("#"+cardName, comment)
+        slack_api.message(cardName,comment)
+        
     return jsonify(data)
 
 if __name__ == '__main__':
